1b0d2b767c3c203e46cf22759f1acf608f695cb0
dupuis brian <dupuisbrian3@gmail.com>
2026-01-19 09:51:28 +0100

    ajout pass world

--- init.sql
+++ init.sql
@@ -1,8 +1,9 @@
 -- Create a simple table for demonstration
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -12,14 +13,16 @@ CREATE TABLE IF NOT EXISTS products (
     price DECIMAL(10, 2) NOT NULL
 );
 
--- Insert some sample data
-INSERT INTO users (username, email) VALUES
-('alice', 'alice@example.com'),
-('bob', 'bob@example.com'),
-('charlie', 'charlie@example.com');
+
+
+-- Insertion avec mot de passe
+INSERT INTO users (username, email, password) VALUES
+('alice', 'alice@example.com', 'alicia'),
+('bob', 'bob@example.com', 'bobi'),
+('charlie', 'charlie@example.com', 'deterre');
 
 INSERT INTO products (name, price) VALUES
 ('Laptop', 1200.00),
 ('Mouse', 25.00),
-('Keyboard', 50.00);
-('My soul', 0.0)
+('Keyboard', 50.00),
+('My soul', 0.0);
